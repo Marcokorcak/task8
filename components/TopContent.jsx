51613c0167ae2f7b205d0328300d665e8bc3060b
Marco Korcak <marcokorcak@Marcos-MacBook-Air.local>
2023-04-26 15:28:34 -0400

    added Image tag

--- components/TopContent.jsx
+++ components/TopContent.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { getCurrentUser } from "../utils/data";
 
 const { name, avatar } = getCurrentUser();
@@ -5,12 +6,8 @@ const { name, avatar } = getCurrentUser();
 const TopContent = () => {
   return (
     <div className="contentCenter">
-      {avatar && (
-        <img className="profilePic"
-          src={avatar}
-          alt={name}
-        />
-      )}
+      {avatar && <Image className="profilePic" src={avatar} alt={name} height="145"
+          width="145"/>}
       {name && <h1 className=" h1">{name}</h1>}
     </div>
   );
